357c68600acf7be134dcd4d3a9f88fb556b1da8d
David Orona <OronaDavidJ@JohnDeere.com>
2020-12-10 13:43:52 -0600

    Some more deck routes

--- controllers/decksController.js
+++ controllers/decksController.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const Deck = require("../models").Deck;
 const User = require("../models").User;
 const Card = require("../models").Card;
+const Stat = require("../models").Stat;
 
 // GET USERS DECKS
 router.get("/", async (req, res) => {
@@ -12,7 +13,42 @@ router.get("/", async (req, res) => {
   let user = await User.findByPk(req.user.id, {
     include: [{ model: Deck }]
   });
-  res.json({ user });
+
+  let decks = await Deck.findAll({
+    include: [{ model: Card }],
+    where: { userId: req.user.id }
+  })
+  res.json({ decks });
 });
+
+// GET USER DECK
+router.get("/:id", async (req, res) => {
+  let deck = await Deck.findByPk(req.params.id, {
+    include: [{ model: Card }],
+    where: { userId: req.user.id }
+  });
+  res.json({ deck });
+});
+
+// router.get("/cards", async (req, res) => {
+//   let cards = await Card.findAll({ include: Stat });
+//   res.json({ cards });
+// });
+
+// ADD CARD TO USER'S DECK
+router.post("/:id/addcard", async (req, res) => {
+  let deck = await Deck.findByPk(req.params.id, {
+    include: [{ model: Card }],
+    where: { userId: req.user.id }
+  });
+
+  let card = await Card.findByPk()
+
+  user.addCard()
+  res.json({ user, card })
+});
+
+// REMOVE CARD FROM USER'S DECK
+
 
 module.exports = router;
