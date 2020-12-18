fb81280deb5d301846d86bad155a420646d8f04c
David Orona <OronaDavidJ@JohnDeere.com>
2020-12-17 19:42:25 -0600

    Put seed data back as example

--- seeders/20201211025908-demo-deckcards.js
+++ seeders/20201211025908-demo-deckcards.js
@@ -6,12 +6,12 @@ module.exports = {
       { deckId: 1, cardId: 1 },
       { deckId: 1, cardId: 2 },
       { deckId: 1, cardId: 3 },
-      // { deckId: 2, cardId: 4 },
-      // { deckId: 2, cardId: 5 },
-      // { deckId: 2, cardId: 6 },
-      // { deckId: 3, cardId: 7 },
-      // { deckId: 3, cardId: 8 },
-      // { deckId: 3, cardId: 9 },
+      { deckId: 2, cardId: 4 },
+      { deckId: 2, cardId: 5 },
+      { deckId: 2, cardId: 6 },
+      { deckId: 3, cardId: 7 },
+      { deckId: 3, cardId: 8 },
+      { deckId: 3, cardId: 9 },
     ]);
   },
 
